6077d68bf98bb1afb0823c85bb30edf2cae2ccb5
Hadj Sassi Mahdi <74431887+HadjSassi@users.noreply.github.com>
2024-07-25 21:53:38 +0000

    Backend update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Home from './Pages/Home';
+import axios from 'axios';
 import Navbar from './Component/Navbar';
+import Home from './Pages/Home';
 import Acceuil from './Pages/Acceuil';
 import NewsArticle from './Pages/NewsArticle';
-import { useState } from 'react';
-import NewsData from './Data/News_Data';
 import Industry from './Pages/Industry';
-import IndustryData from './Data/Industry_Data';
 import OfferCard from './Pages/OfferCard';
 import About from './Pages/About';
 import Signup from './Pages/Signup';
@@ -16,36 +13,57 @@ import Profile from './Pages/Profile';
 import PlanIntegration from './Pages/PlanIntegration';
 import Skillpage from './Pages/Skillpage';
 import Question from './Pages/Question';
+import NewPlan from './Pages/NewPlan';
 import Brouillon from './Brouillon';
+import './App.css';
 
 function App() {
+    const [newsData, setNewsData] = useState([]);
+    const [industryData, setIndustryData] = useState([]);
 
-  const [newsData] = useState(NewsData);
-  const [industryData] = useState(IndustryData);
- 
-  return (
-    <div className="App">
-    
-         <Navbar/>
+    useEffect(() => {
+        const fetchNewsData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/news');
+                setNewsData(response.data);
+            } catch (error) {
+                console.error('Error fetching news data:', error);
+            }
+        };
 
-         <div>
-          <Routes>
-          
-           <Route path="/Acceuil"  element={<Acceuil  />}/>
-           <Route path="/Article/:id"  element={<NewsArticle newsData={newsData} />}/>
-           <Route path="/Industry/:id/:offer_id"  element={<Industry industryData={industryData}   />}/>
-           <Route path="/About"  element={<About  />}/>
-           <Route path="/Signup"  element={<Signup  />}/>
-           <Route path="/Profile"  element={<Profile  />}/>
-           <Route path="/PlanIntegration"  element={<PlanIntegration  />}/>
-           <Route path="/Skillpage/:id"     element={<Skillpage/>} />
-           <Route path="/Question"     element={<Question/>} />
-          
-          </Routes>
-         </div>
-       
-    </div>
-  );
+        const fetchIndustryData = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/api/industry');
+                setIndustryData(response.data);
+            } catch (error) {
+                console.error('Error fetching industry data:', error);
+            }
+        };
+
+        fetchNewsData();
+        fetchIndustryData();
+    }, []);
+
+    return (
+        <div className="App">
+            <Navbar />
+
+            <div>
+                <Routes>
+                    <Route path="/Acceuil" element={<Acceuil />} />
+                    <Route path="/Article/:id" element={<NewsArticle newsData={newsData} />} />
+                    <Route path="/Industry/:id/:offer_id" element={<Industry industryData={industryData} />} />
+                    <Route path="/About" element={<About />} />
+                    <Route path="/Signup" element={<Signup />} />
+                    <Route path="/Profile" element={<Profile />} />
+                    <Route path="/PlanIntegration" element={<PlanIntegration />} />
+                    <Route path="/Skillpage/:id" element={<Skillpage />} />
+                    <Route path="/Question" element={<Question />} />
+                    <Route path="/add-plan" element={<NewPlan />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
